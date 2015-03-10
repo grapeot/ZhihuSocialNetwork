1f88c2c2cc13a8919147547aa87c6c705520b22e
Yan Wang <grapeot@gmail.com>
2015-03-09 20:27:32 -0400

    Now we can crawl timeline incrementally. Fix #3.

--- src/crawlTimeline.py
+++ src/crawlTimeline.py
@@ -9,31 +9,36 @@ def crawlTimeline(userid):
     apiurl = 'http://www.zhihu.com/people/{0}/activities'.format(userid)
     params = 'start={0}&_xsrf=c4b07884cfea379e46cdcb89fcf08cc4'
     timestamp = int(time.time())
-    isContinue = True
     mongoToWrite = []
 
-    while isContinue:
+    # determine the latest timestamp.
+    # we don't need to crawl after reaching the this one
+    client = MongoClient()
+    users = client['zhihu']['users']
+    user = users.find_one({'name': userid})
+    if 'likes' not in user:
+        user.update({'likes': []})
+    latestTimestamp = max([int(x['timestamp']) for x in user['likes']])
+
+    while True:
         content = util.postZhihu(apiurl, params.format(timestamp))
         content = json.loads(content)
         answers = re.findall(r'data-time="(\d+)"[^>]*>\s<span[^>]*>[^<]*</span>\s<div[^>]*>\s<a[^>]*>[^<]*</a>[^<]*<a class="question_link" target="_blank" href="/question/(\d+)/answer/(\d+)">', content['msg'][1])
         mongoToWrite += [{'timestamp': x[0], 'qid': x[1], 'aid': x[2]} for x in answers] 
         # whether to terminate the crawling
         remainingMsgNum = int(content['msg'][0])
-        if remainingMsgNum < 20:
+        earliestTimestamp = min([int(x['timestamp']) for x in mongoToWrite])
+        if remainingMsgNum < 20 or earliestTimestamp < latestTimestamp:
             break
         timestamp = int(re.findall('data-time="([^"]*)"', content['msg'][1])[-1])
 
     # update the database
-    client = MongoClient()
-    users = client['zhihu']['users']
-    user = users.find_one({'name': userid})
-    if 'likes' not in user:
-        user.update({'likes': []})
     # do deduplication
     oldTimestamps = set([x['timestamp'] for x in user['likes']])
     mongoToWrite = [x for x in mongoToWrite if x['timestamp'] not in oldTimestamps]
     user['likes'] += mongoToWrite
     users.update({'name': userid}, user)
+    sys.stdout.write('[{1}] {0} new entries written to the database.\n'.format(len(mongoToWrite), userid))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
