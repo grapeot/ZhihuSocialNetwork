9f3339e42055a9be5028ae5a563946495d0e5706
Yan Wang <grapeot@gmail.com>
2015-03-14 17:34:22 -0400

    Add a script to print questions.

---
+++ src/printQuestions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+""" Print all the question ids which don't appear in the database """
+from pymongo import MongoClient
+
+def printQuestions():
+    client = MongoClient()
+    setQidsFromUsers = set( [ y['qid'] for x in client['zhihu']['users'].find() for y in x['likes'] ] )
+    setQidsFromQuestions = set( [x['qid'] for x in client['zhihu']['questions'].find() ] )
+    for qid in setQidsFromUsers - setQidsFromQuestions:
+        print qid
+
+if __name__ == '__main__':
+    printQuestions()
