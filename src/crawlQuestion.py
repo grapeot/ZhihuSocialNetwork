3ff912619269550f664a77252e4b52971a7f577e
Yan Wang <grapeot@gmail.com>
2015-03-14 19:50:05 -0400

    Add author processing capability to question crawling.

--- src/crawlQuestion.py
+++ src/crawlQuestion.py
@@ -2,6 +2,7 @@
 
 import util
 import sys, time, re
+from BeautifulSoup import BeautifulSoup
 from pymongo import MongoClient
 
 def crawlQuestion(qid):
@@ -9,7 +10,7 @@ def crawlQuestion(qid):
     print '[{0}] starts.'.format(qid)
     apiurl = 'http://www.zhihu.com/question/{0}'.format(qid)
     client = MongoClient()
-    content = util.getZhihu(apiurl.format(qid), includeCookie=False)
+    content = util.getZhihu(apiurl.format(qid))
 
     # process the question itself
     topicids = re.findall(r'"/topic/(\d+)"', content)
@@ -23,13 +24,21 @@ def crawlQuestion(qid):
     upvotes = [int(x) for x in re.findall(r'data-votecount="(\d+)"', content)]
     dateCreateds = [int(x) for x in re.findall(r'data-created="(\d+)"', content)]
     scores = [float(x) for x in re.findall(r'data-score="([0-9+-.]*)"', content)]
-    
-    #authorTexts = re.findall('<h3 class="zm-item-answer-author-wrap">.*?</h3>', content)
+    authorTexts = BeautifulSoup(content).findAll('h3', { 'class': 'zm-item-answer-author-wrap' })
+    def parseAuthor(text):
+        match = re.search('/people/([^"]*)', text)
+        if match: # visible users
+            return match.group(1)
+        match = re.search('<h3 [^>]*>([^<]*)</h3>', text)
+        if match: # anonymous
+            return match.group(1)
+        return 'UNKNOWN' # invisible
+    authors = [parseAuthor(str(x)) for x in authorTexts]
     print len(topAnswerIds), topAnswerIds
     print len(upvotes), upvotes
     print len(dateCreateds), dateCreateds
     print len(scores), scores
-    #print len(authors), authors
+    print len(authors), authors
     toInsert = []
     for i in range(len(upvotes)):
         toInsert.append({
@@ -38,7 +47,8 @@ def crawlQuestion(qid):
             'lastQuestionCrawlTimestamp': timestamp,
             'upvote': upvotes[i],
             'dateCreated': dateCreateds[i],
-            'score': scores[i]
+            'score': scores[i],
+            'author': authors[i]
         })
     for r in toInsert:
         client['zhihu']['answers'].update({ 'id': r['id'] }, r, upsert=True)
