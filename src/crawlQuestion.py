41e721b047db6ba360e7d8dab4f0d7f160810b88
Yan Wang <grapeot@gmail.com>
2015-03-14 18:44:24 -0400

    also add upvote update of answers to the crawlQuestion.py

--- src/crawlQuestion.py
+++ src/crawlQuestion.py
@@ -6,16 +6,26 @@ from pymongo import MongoClient
 
 def crawlQuestion(qid):
     """ Crawl the topics of the question, and store the result in the database. """
+    print '[{0}] starts.'.format(qid)
     apiurl = 'http://www.zhihu.com/question/{0}'.format(qid)
     client = MongoClient()
     content = util.getZhihu(apiurl.format(qid), includeCookie=False)
+
+    # process the question itself
     topicids = re.findall(r'"/topic/(\d+)"', content)
     timestamp = int(time.time())
     title = re.search('<h2 class="zm-item-title zm-editable-content">([^<]*)', content).group(1).strip()
     topAnswerIds = list(set(re.findall('"/question/{0}/answer/(\d+)"'.format(qid), content)))
     toInsert = { 'id': qid, 'title': title, 'topicIds': topicids, 'lastCrawlTimestamp': timestamp, 'topAnswerIds': topAnswerIds }
-    print toInsert
     client['zhihu']['questions'].update({ 'id': qid }, toInsert, upsert=True)
+
+    # process the answers
+    upvotes = [int(x) for x in re.findall(r'data-votecount="(\d+)"', content)]
+    toInsert = [{ 'id': aid, 'qid': qid, 'lastQuestionCrawlTimestamp': timestamp, 'upvote': upvote }
+        for aid, upvote in zip(topAnswerIds, upvotes)]
+    for r in toInsert:
+        client['zhihu']['answers'].update({ 'id': r['id'] }, r, upsert=True)
+    print '[{0}] complete, {1} answers updated.'.format(qid, len(upvotes))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
