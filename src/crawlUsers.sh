7b7e330a23faba3d5dc6c7e2aaeef65067f5171b
Yan Wang <grapeot@gmail.com>
2015-03-15 23:36:08 -0400

    Make the crawling script more robust (against the SIGPIPE)

--- src/crawlUsers.sh
+++ src/crawlUsers.sh
@@ -1,1 +1,2 @@
-python ./printNames.py 3 | xargs -P 10 -I{} -n 1 bash -c 'python ./crawlTimeline.py {}'
+python ./printNames.py 3 > usernames.txt
+cat usernames.txt | xargs -P 10 -I{} -n 1 bash -c 'python ./crawlTimeline.py "{}"'
