d0f09a123eb8e3e459332e4220f45238d32a3687
Yan Wang <grapeot@gmail.com>
2015-03-21 00:25:08 -0400

    Revise the deploy script.

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,12 +1,8 @@
-sudo apt-get install mongodb-server python-bs4 python-numpy python-scipy python-requests
+sudo apt-get install mongodb-server python-bs4 python-numpy python-scipy python-requests 
 pip install -U pymongo beautifulsoap
 
 # install scikit-learn
-sudo apt-get install build-essential python-dev python-setuptools \
-    python-numpy python-scipy \
-    libatlas-dev libatlas3gf-base
-sudo update-alternatives --set libblas.so.3 \
-        /usr/lib/atlas-base/atlas/libblas.so.3
-sudo update-alternatives --set liblapack.so.3 \
-        /usr/lib/atlas-base/atlas/liblapack.so.3
+sudo apt-get install build-essential python-dev python-setuptools python-numpy python-scipy python-matplotlib libatlas-dev libatlas3gf-base
+sudo update-alternatives --set libblas.so.3 /usr/lib/atlas-base/atlas/libblas.so.3
+sudo update-alternatives --set liblapack.so.3 /usr/lib/atlas-base/atlas/liblapack.so.3
 pip install --user --install-option="--prefix=" -U scikit-learn
