3263d948c3bc6cbddda2dd241a53087cec824d2a
Yan Wang <grapeot@gmail.com>
2015-03-21 00:46:07 -0400

    update the user crawling shell script

--- src/crawlUsers.sh
+++ src/crawlUsers.sh
@@ -1,2 +1,2 @@
-python ./printNames.py 3 > usernames.txt
-cat usernames.txt | xargs -P 10 -I{} -n 1 bash -c 'python ./crawlTimeline.py "{}"'
+python ./printNames.py 3 > ../dat/usernames.txt
+cat ../dat/usernames.txt | xargs -P 10 -I{} -n 1 bash -c 'python ./crawlTimeline.py "{}"'
