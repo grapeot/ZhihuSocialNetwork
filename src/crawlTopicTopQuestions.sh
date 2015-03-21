882a292c27d1589c2e6b57d2d989980ae10ddda1
Yan Wang <grapeot@gmail.com>
2015-03-21 00:42:54 -0400

    add a script to extend new authors from top questions of each topic

--- src/crawlTopicTopQuestions.sh
+++ src/crawlTopicTopQuestions.sh
@@ -1,1 +1,1 @@
-python ./printTopics.py 0 | xargs -n 1 -I{} -P 15 bash -c 'python ./crawlTopicTopQuestions.py "{}"'
+python ./printTopics.py 0.5 | xargs -n 1 -I{} -P 15 bash -c 'python ./crawlTopicTopQuestions.py "{}"'
---
+++ src/extendAuthorsFromTopQuesitons.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+questions = client['zhihu']['questions']
+
+# find all top questions
+topQuestions = [y for x in client['zhihu']['topics'].find({ 'topQuestionIds': { '$exists': 1 } }, { 'topQuestionIds': 1 }) for y in x['topQuestionIds'] ]
+print len(topQuestions), ' top questions are found.'
+topQuestions = set(topQuestions)
+print len(topQuestions), ' top questions are found after deduplication.'
+# find authors 
+def findAuthor(qid):
+    q = client['zhihu']['questions'].find_one({ 'id': qid, 'topAnswerIds': { '$exists' : 1 } })
+    answerId = int(q['topAnswerIds'][0]) if (q and 0 != len(q['topAnswerIds'])) else None
+    if answerId is None:
+        return []
+    answer = client['zhihu']['answers'].find_one({'id': answerId})
+    return [] if answer is None else [answer['author']]
+authors = [y for qid in topQuestions for y in findAuthor(qid)]
+print len(authors), ' authors are found.'
+# add authors to the database
+oldAuthors = [y['name'] for y in client['zhihu']['users'].find()]
+authorsToAdd = set(authors) - set(oldAuthors)
+print len(authorsToAdd), ' authors will be added.'
+file('../dat/authors.txt', 'w').write('\n'.join([x for x in authorsToAdd]).encode('utf-8'))
