d52672d3547b8b24f1d9b1c445f3faeda111b64c
Yan Wang <grapeot@gmail.com>
2015-03-21 10:32:25 -0400

    update some shell scripts

--- src/extendAuthorsFromTopQuesitons.py
+++ src/extendAuthorsFromTopQuesitons.py
@@ -18,8 +18,22 @@ def findAuthor(qid):
     return [] if answer is None else [answer['author']]
 authors = [y for qid in topQuestions for y in findAuthor(qid)]
 print len(authors), ' authors are found.'
-# add authors to the database
+
+# figure out the authors to be added to the database
 oldAuthors = [y['name'] for y in client['zhihu']['users'].find()]
 authorsToAdd = set(authors) - set(oldAuthors)
 print len(authorsToAdd), ' authors will be added.'
 file('../dat/authors.txt', 'w').write('\n'.join([x for x in authorsToAdd]).encode('utf-8'))
+print len(authorsToAdd), 'authors has been serialized to ../dat/authors.txt'
+
+# add the authors to the database
+print 'Add to the database? (y/n)',
+choice = raw_input()
+if 'y' == choice or 'Y' == choice:
+    for a in authorsToAdd:
+        client['zhihu']['users'].insert({
+            'name': a,
+            'lastCrawlTimestamp': 0,
+            'likes': []
+        });
+    print 'done.'
