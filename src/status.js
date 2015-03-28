01fce007feffef9ea7ebe402572f3f96af99abe2
Yan Wang <grapeot@gmail.com>
2015-03-28 16:54:43 -0400

    add a mongodb monitoring utility

---
+++ src/status.js
@@ -0,0 +1,7 @@
+conn = new Mongo();
+db = conn.getDB('zhihu');
+print('authors: ' + db.users.count() +
+        ' questions: ' + db.questions.count() + 
+        ' answers: ' + db.answers.count() + 
+        ' topic: ' + db.topics.count() +
+        '\ncrawledAnswers: ' + db.answers.find({'content': {'$exists': 1}}).count());
